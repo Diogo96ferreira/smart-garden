ab03e6dce60332b8e231d3cadb0e064df53a037b
Diogo Ferreira <diogo96ferreira@gmail.com>
2025-11-11 11:33:06 +0000

    removing icon from onboarding step plants

--- src/app/[locale]/onboarding/steps/StepPlants.tsx
+++ src/app/[locale]/onboarding/steps/StepPlants.tsx
@@ -301,7 +301,6 @@ export function StepPlants({ onBack, onNext }: Props) {
         <div className="relative">
           <Input
             id="plant-search"
-            leadingIcon={<Search className="h-4 w-4" />}
             placeholder="Exemplo: Tomate cereja, Manjericao, Morango..."
             value={searchTerm}
             onChange={(event) => {
